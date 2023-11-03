26a39486eca56bdffd1563ee3ce0a21fef0963a6
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2023-11-03 22:59:17 +0700

    Membuat fungsi header output

---
+++ clientdata.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void header(){
+	std::cout << "=========================" << "\n";
+	std::cout << "===    BANK LABIRIN   ===" << "\n";
+	std::cout << "=========================" << "\n";
+}
+int main(){
+	header();
+	return 0;
+}
