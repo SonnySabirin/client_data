026843c241a97881ede41a9c553b98fb6747acc3
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2023-11-04 17:12:55 +0700

    Inisialisasi node nasabah

--- clientdata.cpp
+++ clientdata.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+struct Nasabah{
+	std::string nama_lengkap;
+	std::string no_rekening;
+	std::string no_kartu_atm;
+	std::string jenis_akun;
+	std::string tanggal_lahir;
+	std::string tempat_lahir;
+	std::string email;
+	std::string no_telepon;
+	std::string jenis_kelamin;
+	std::string alamat;
+	std::string nama_ibu;
+	std::string profesi;
+	Nasabah* next;
+}
+
 void header(){
 	std::cout << "=========================" << "\n";
 	std::cout << "===    BANK LABIRIN   ===" << "\n";
@@ -12,6 +28,7 @@ void menu_utama(){
 	std::cout << "2. Input data nasabah baru" << "\n";
 	std::cout << "3. Keluar" << "\n";
 }
+
 int main(){
 	menu_utama();
 	return 0;
